2d84487f5334cc042cf831338b173bc9f561d49c
Hwangho <hwangho6459@gmail.com>
2022-11-07 14:22:44 +0900

    Initial Commit

---
+++ StudyMate/Source/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudyMate
+//
+//  Created by 송황호 on 2022/11/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
