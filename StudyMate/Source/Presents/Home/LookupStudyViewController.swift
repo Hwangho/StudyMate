48ac151f41a4bd7aedeb469e676147aca017fac0
Hwangho <hwangho6459@gmail.com>
2022-11-27 15:17:23 +0900

    [fix] code 수정

---
+++ StudyMate/Source/Presents/Home/LookupStudyViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LookupStudyViewController.swift
+//  StudyMate
+//
+//  Created by 송황호 on 2022/11/27.
+//
+
+import UIKit
+
+import SnapKit
+import RxSwift
+import RxCocoa
+
+
+final class LookupStudyViewController: BaseViewController {
+    
+    
+
+    /// Life Cycle
+    override func setupAttributes() {
+        super.setupAttributes()
+    }
+
+    override func setupLayout() {
+        <#code#>
+    }
+}
